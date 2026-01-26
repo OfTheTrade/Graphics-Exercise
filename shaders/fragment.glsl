a742ddd2aea82bdb9deeb575b21c33b6278c440a
OfTheTrade <alexisorlandos2005@gmail.com>
2026-01-26 12:57:36 +0200

    Added distance factor to fragment shader

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -36,11 +36,11 @@ void main()
     else {
         // --- CUBE ---
         // I reuse the planets mtl data, looks good enough
-        // Some of the values where unused anyway since it's a light source
+        // Some of the values where unused anyway since it's a light source so I use them here
         vec4 texColor = texture(cube, TexCoord);
 
         // Ambient
-        vec3 ambient = material.ambient * texColor.rgb;
+        vec3 ambient = (material.ambient + 0.2) * texColor.rgb;
 
         // Diffuse (The Cube faces the Planet)
         vec3 norm = normalize(Normal);
@@ -48,11 +48,24 @@ void main()
         float diff = max(dot(norm, lightDir), 0.0);
         vec3 diffuse = (diff * material.diffuse) * texColor.rgb;
 
-        // Specular (Shine)
+        // Specular
         vec3 viewDir = normalize(viewPos - FragPos);
         vec3 reflectDir = reflect(-lightDir, norm);  
         float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
         vec3 specular = (spec * material.specular);
+
+        // Distance reduction
+        float constant = 1.0;  
+        float linear = 0.045;   
+        float quadratic = 0.0075; 
+        
+        // Calculate f(d)
+        float dist = length(lightPos - FragPos);
+        float attenuation = 1.0 / (constant + linear * dist + quadratic * (dist * dist));
+
+        // Apply f(d)
+        diffuse  *= attenuation;
+        specular *= attenuation;
 
         vec3 result = ambient + diffuse + specular;
         FragColor = vec4(result, texColor.a);
